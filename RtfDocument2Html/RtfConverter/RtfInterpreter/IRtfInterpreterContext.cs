0b4be5dd77deeede21504159676bf17bf3b0e7e2
edenbuaa <edenbuaa@outlook.com>
2016-11-03 17:34:47 +0800

    Add files via upload

---
+++ RtfDocument2Html/RtfConverter/RtfInterpreter/IRtfInterpreterContext.cs
@@ -0,0 +1,51 @@
+﻿
+namespace RtfConverter.RtfInterpreter
+{
+
+	// ------------------------------------------------------------------------
+	public interface IRtfInterpreterContext
+	{
+
+		// ----------------------------------------------------------------------
+		RtfInterpreterState State { get; }
+
+		// ----------------------------------------------------------------------
+		int RtfVersion { get; }
+
+		// ----------------------------------------------------------------------
+		string DefaultFontId { get; }
+
+		// ----------------------------------------------------------------------
+		IRtfFont DefaultFont { get; }
+
+		// ----------------------------------------------------------------------
+		IRtfFontCollection FontTable { get; }
+
+		// ----------------------------------------------------------------------
+		IRtfColorCollection ColorTable { get; }
+
+		// ----------------------------------------------------------------------
+		string Generator { get; }
+
+		// ----------------------------------------------------------------------
+		IRtfTextFormatCollection UniqueTextFormats { get; }
+
+		// ----------------------------------------------------------------------
+		IRtfTextFormat CurrentTextFormat { get; }
+
+		// ----------------------------------------------------------------------
+		IRtfTextFormat GetSafeCurrentTextFormat();
+
+		// ----------------------------------------------------------------------
+		IRtfTextFormat GetUniqueTextFormatInstance( IRtfTextFormat templateFormat );
+
+		// ----------------------------------------------------------------------
+		IRtfDocumentInfo DocumentInfo { get; }
+
+		// ----------------------------------------------------------------------
+		IRtfDocumentPropertyCollection UserProperties { get; }
+
+	} // interface IRtfInterpreterContext
+
+} 
+
---
+++ RtfDocument2Html/RtfConverter/RtfInterpreter/RtfInterpreterState.cs
@@ -0,0 +1,15 @@
+﻿
+namespace RtfConverter.RtfInterpreter
+{
+
+	// ------------------------------------------------------------------------
+	public enum RtfInterpreterState
+	{
+		Init,
+		InHeader,
+		InDocument,
+		Ended
+	} // enum RtfInterpreterState
+
+} 
+
